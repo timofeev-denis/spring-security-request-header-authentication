b06c6b27ed0f52ee76907a5040734e5e31939742
Denis Timofeev <nakolenke@gmail.com>
2020-01-28 11:20:34 +0300

    Create project

---
+++ src/main/java/ru/code4fun/demo/headerauthentication/HeaderAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.headerauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeaderAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeaderAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/code4fun/demo/headerauthentication/HeaderAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.headerauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeaderAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
